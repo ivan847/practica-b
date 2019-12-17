7dbd3b69b7dc52c30f45e41d6806630140f37ba6
Usuario <Usuario@Ivn>
2019-12-17 07:53:03 -0500

    hola m

---
+++ src/practicab/usapractica.java
@@ -0,0 +1,11 @@
+
+package practicab;
+
+
+public class usapractica {
+    public static void main(String[] args) {
+        practica p=new practica();
+        p.setVisivle(true);
+        p.setLocationRelative(null);
+    }
+}
